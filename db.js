cfb4f23eee7db7804e54c758f6e0332782edc366
Shreyeska <silwalshreyeska@gmail.com>
2023-12-21 23:56:55 +0545

    Mongodb installation

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 //  for user login and authentication
 const mongoose= require("mongoose");
-const connect = mongoose.connect("mongodb+srv://jasmineb:NawaAama@cluster0.hdnidxc.mongodb.net/?retryWrites=true&w=majority");
-
+//const connect = mongoose.connect("mongodb+srv://jasmineb:NawaAama@cluster0.hdnidxc.mongodb.net/?retryWrites=true&w=majority");
+const connect = mongoose.connect("mongodb+srv://Shreyeska:ITeGL2pAv6JRkmsi@cluster0.4bw0byn.mongodb.net/")
 //check connection
 connect.then(()=>{
     console.log("Database connected Successfully")
