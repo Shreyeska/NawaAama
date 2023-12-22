42f6cb51d5e59c180d3a4b55bbb3a42d5adfa293
Shreyeska <silwalshreyeska@gmail.com>
2023-12-22 18:13:11 +0545

    Added hyperlinks to community page

--- db.js
+++ db.js
@@ -2,6 +2,7 @@
 const mongoose= require("mongoose");
 const connect = mongoose.connect("mongodb+srv://jasmineb:NawaAama@cluster0.hdnidxc.mongodb.net/?retryWrites=true&w=majority");
 
+
 //check connection
 connect.then(()=>{
     console.log("Database connected Successfully")
